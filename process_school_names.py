ffc48810bfed9c2c95c7a89106f83df4251edf2f
Paul Beard <paulbread88@gmail.com>
2025-06-06 18:28:11 +0100

    add a prompt

--- process_school_names.py
+++ process_school_names.py
@@ -12,7 +12,7 @@ data_raw = pd.read_csv(
 )
 
 # %%
-
 data_raw["school_clean"] = data_raw["school_raw"].str.extract(r"^(.*?),")
 
 # %%
+prompt = "find me the contact details for the following places. For each place, return a JSON file with the 'phone', 'email' and 'address' fields populated. If you cannot find anything leave as 'None'."
